7faebe308db69dc19651de0869b2fd88883d7f5b
Sverxsro4nik <Sverxsro4nik@gmail.com>
2021-11-15 21:54:56 +0500

    создание компонента для описания работ и массива данных для двух работ

--- src/components/SectionAboutWork/SectionAboutWork.jsx
+++ src/components/SectionAboutWork/SectionAboutWork.jsx
@@ -1,37 +1,15 @@
 import React from "react";
-import design from "../../img/design.webp";
-import table from "../../img/table.webp";
-import Line from "../Line/Line";
+import data from "../../data/works";
+import AboutWort from "../AboutWork/AboutWork";
 
 const SectionAboutWork = () => {
     return(
         <section className="about-work">
-            <div className="about-work__element section-backgroundYellow">
-                <img src={design} alt=""/>
-                <div className="about-work__element__text">
-                    <h4>Our Design Story</h4>
-                    <Line lineClass="line"/>
-                    <p>
-                        We believe in thoughtful, intentional living. So we promise not to clutter your cabinets
-                        with one-off pieces you’ll rarely use.
-                        Our collection only includes items you’ll reach for every day.
-                        Because mindful living should be simple, not stressful.
-                    </p>
-                </div>
-            </div>
-            <div className="about-work__element section-backgroundYellow">
-                <img src={table} alt=""/>
-                <div className="about-work__element__text">
-                    <h4>Your Table Your Time</h4>
-                    <Line lineClass="line"/>
-                    <p>
-                        Our modern lives keep us in constant motion.
-                        But what keeps us moving isn’t always what moves us.
-                        Our time at the table is our pause button. Meals can become moments and moments can become memories.
-                        So grab a plate, pull up a seat, and stay a while
-                    </p>
-                </div>
-            </div>
+            {
+                data.map((work, key) => {
+                    return <AboutWort title = {work.title} text={work.text} src={work.src} key={work.src}/>
+                })
+            }
         </section>
     );
 }
---
+++ src/data/works.js
@@ -0,0 +1,17 @@
+import design from '../img/design.webp';
+import table from '../img/table.webp';
+
+const works = [
+    {
+        title: 'Our Design Story',
+        text: 'We believe in thoughtful, intentional living. So we promise not to clutter your cabinets with one-off pieces you’ll rarely use. Our collection only includes items you’ll reach for every day. Because mindful living should be simple, not stressful.',
+        src: design
+    },
+    {
+        title: 'Your Table Your Time',
+        text: 'Our modern lives keep us in constant motion. But what keeps us moving isn’t always what moves us. Our time at the table is our pause button. Meals can become moments and moments can become memories. So grab a plate, pull up a seat, and stay a while',
+        src: table
+    }
+];
+
+export default works;
